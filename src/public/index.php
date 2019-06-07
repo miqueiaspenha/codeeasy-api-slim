b2a83486da36e8f81dd6e9c6991408911fca762b
Miqueias Penha <miqueiaspenha@gmail.com>
2019-06-07 20:44:48 -0300

    add middleware and group routes

--- src/public/index.php
+++ src/public/index.php
@@ -12,16 +12,38 @@ $config = [
     ],
 ];
 
+$mid01 = function (Request $request, Response $response, $next): Response
+{
+    $response->getBody()->write('Dentro do middleware 01<br>');
+    $response = $next($request, $response);
+    $response->getBody()->write('<br>Dentro do middleware 02');
+    return $response;
+};
+
 $configuration = new Container($config);
 
 $app = new App($configuration);
+
+$app->group('/users', function () use ($app)
+{
+    $app->get('/', function (Request $request, Response $response, array $args): Response
+    {
+        $response->getBody()->write('User');
+        return $response;
+    });
+    $app->get('/list', function (Request $request, Response $response, array $args): Response
+    {
+        $response->getBody()->write('List Users');
+        return $response;
+    });
+})->add($mid01);
 
 $app->get('/produto[/{nome}]', function (Request $request, Response $response, array $args): Response {
     $limit = $request->getQueryParams()['limit'] ?? 10;
     $nome = $args['nome'] ?? 'feijao';
     $response->getBody()->write('Produto ' . $nome . ' do banco de dados tem ' . $limit . ' unidades.');
     return $response;
-});
+})->add($mid01);
 
 $app->post('/produto', function (Request $request, Response $response, array $args): Response {
     $data = $request->getParsedBody();
